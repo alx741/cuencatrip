93055c504a4132aedcf67a85fe558e4e08749428
Daniel Campoverde <alx@sillybytes.net>
2017-07-04 12:05:57 -0500

    Extract coordinates query parameters reading

--- src/Handler/Places.hs
+++ src/Handler/Places.hs
@@ -23,8 +23,8 @@ import Place
 
 data PlaceData = PlaceData
     { placeName :: String
-    , placeLat :: Float
-    , placeLng :: Float
+    , placeLat :: Double
+    , placeLng :: Double
     , placeRating :: Float
     } deriving (Generic, Show)
 
@@ -40,37 +40,43 @@ data Coordinate = Coordinate
 instance ToJSON Coordinate
 instance FromJSON Coordinate
 
-toCoordinate :: Parser Coordinate
-toCoordinate = do
+getPlacesR :: Place -> Handler Value
+getPlacesR _ = do
+    let sql = "select name, raters, rating, ST_AsText(shop.geog) from shop "
+            ++ "where ST_Distance(shop.geog, ST_Point(-79.00789260776946, -2.9026843924063)) < 1000"
+            -- "AND shop.shop= 'supermarket'"
+    runDB $ rawQuery sql [] $$ CL.mapM_ (liftIO . print)
+    coordinate <- case parse coordinateParser "" "POINT(-79.0209324 -2.8976525)" of
+            Left err -> notFound
+            Right coor -> return coor
+    return $ toJSON coordinate
+
+coordinateParser :: Parser Coordinate
+coordinateParser = do
     _ <- string "POINT("
     lng <- ap sign fractional
     spaces
     lat <- ap sign fractional
     return $ Coordinate lat lng
 
-getPlacesR :: Place -> Handler Value
-getPlacesR _ = do
-    let sql = "select name, ST_AsText(shop.geog) from shop where gid=100"
-    runDB $ rawQuery sql [] $$ CL.mapM_ (liftIO . print)
-    coordinate <- case parse toCoordinate "" "POINT(-79.0209324 -2.8976525)" of
-            Left err -> notFound
-            Right coor -> return coor
-    return $ toJSON coordinate
-
-
 getCuencaR :: Handler Html
 getCuencaR = do
-    mlat <- lookupGetParam "lat"
-    mlong <- lookupGetParam "long"
-
-    lat <- case mlat of
-            Nothing -> redirect HomeR
-            Just x -> return $ rawJS x
-
-    long <- case mlong of
-            Nothing -> redirect HomeR
-            Just x -> return $ rawJS x
-
+    (lng, lat) <- coordParam >>= (\(a, b) -> return $ (rawJS a, rawJS b))
     defaultLayout $ do
         setTitle "CuencaTrip"
         $(widgetFile "places")
+
+coordParam :: Handler (Text, Text)
+coordParam = do
+    mlat <- lookupGetParam "lat"
+    mlng <- lookupGetParam "long"
+
+    lat <- case mlat of
+            Nothing -> redirect HomeR
+            Just x -> return x
+
+    lng <- case mlng of
+            Nothing -> redirect HomeR
+            Just x -> return x
+
+    return (lng, lat)
